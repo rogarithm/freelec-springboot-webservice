77294647db9666af5668cb8ef1731bddd40fe3c2
sehun-kim <rogarithm@gmail.com>
2023-03-12 22:25:43 +0900

    PostsRepository 테스트 추가

---
+++ src/test/java/com/rogarithm/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.rogarithm.book.springboot.domain.posts;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void savePostAndGet() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("sehoongim@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
